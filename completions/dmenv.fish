915d0fe4dcb5684f27932b52b251537b0cd2bddc
T.S. Foster <hi@tsf.io>
2018-06-25 17:26:49 +0100

    Fix accidental deletion

--- completions/dmenv.fish
+++ completions/dmenv.fish
@@ -1,3 +1,3 @@
 complete --command=dmenv --no-files --description='Set docker-machine environment variables'
 complete --command=dmenv --no-files --arguments='(docker-machine ls -f "{{.Name}}")'
-complete --command=dmenv --short-option=h --long-opion=help='Print help'
+complete --command=dmenv --short-option=h --long-option=help --description='Print help'
